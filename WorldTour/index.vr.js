7b1785af3db99f7413f33d2763682c4f07278ec9
davidkatz <15Dkatz@shcp.edu>
2017-05-13 17:50:50 -0700

    complete worldTour

--- WorldTour/index.vr.js
+++ WorldTour/index.vr.js
@@ -2,12 +2,33 @@ import React, { Component } from 'react';
 
 import { View, Text, Pano, AppRegistry, asset, StyleSheet } from 'react-vr';
 
+const places = [
+  {
+    title: 'Island Paradise',
+    image: 'island-garden.jpg'
+  },
+  {
+    title: 'Starry Night',
+    image: 'starry-sky.jpg'
+  },
+  {
+    title: 'Winter Outdoors',
+    image: 'winter-outdoor.jpg'
+  },
+  {
+    title: 'Light Show',
+    image: 'light-show.jpg'
+  }
+]
+
+
 class WorldTour extends Component {
   constructor() {
     super();
 
     this.state = {
-      showMenu: false
+      showMenu: false,
+      place: 'starry-sky.jpg'
     }
   }
 
@@ -18,7 +39,7 @@ class WorldTour extends Component {
   render() {
     return (
       <View>
-        <Pano source={asset('starry-sky.jpg')}></Pano>
+        <Pano source={asset(this.state.place)}></Pano>
         <View
           style={styles.menuButton}
           onEnter={() => this.toggleMenu()}
@@ -27,12 +48,42 @@ class WorldTour extends Component {
             {this.state.showMenu ? 'Close Menu' : 'Open Menu'}
           </Text>
         </View>
+        {
+          this.state.showMenu ?
+            <View style={styles.menu}>
+              {
+                places.map((place, index) => {
+                  return (
+                    <View
+                      style={styles.menuItem}
+                      key={index}
+                      onEnter={() => this.setState({place: place.image})}
+                    >
+                      <Text style={styles.menuItemText}>{place.title}</Text>
+                    </View>
+                  )
+                })
+              }
+            </View>
+          :
+            <View></View>
+        }
       </View>
     )
   }
 };
 
 const styles = StyleSheet.create({
+  menu: {
+    width: 5,
+    height: 1.25,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    transform: [
+      {translate: [-2, 0, -7.5]}
+    ]
+  },
   menuButton: {
     backgroundColor: '#fff',
     borderRadius: 0.25,
@@ -48,6 +99,20 @@ const styles = StyleSheet.create({
   menuButtonText: {
     textAlign: 'center',
     fontSize: 0.15,
+    color: '#000'
+  },
+  menuItem: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 1,
+    height: 1,
+    borderRadius: 0.5,
+    borderWidth: 0.02,
+    backgroundColor: '#fff'
+  },
+  menuItemText: {
+    fontSize: 0.2,
+    textAlign: 'center',
     color: '#000'
   }
 })
--- WorldTour/vr/client.js
+++ WorldTour/vr/client.js
@@ -8,6 +8,7 @@ import {VRInstance} from 'react-vr-web';
 function init(bundle, parent, options) {
   const vr = new VRInstance(bundle, 'WorldTour', parent, {
     // Add custom options here
+    enableHotReload: true,
     ...options,
   });
   vr.render = function() {
