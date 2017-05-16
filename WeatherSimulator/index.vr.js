5597adcbc015fffab1e4b55df946159d04dbc199
davidkatz <15Dkatz@shcp.edu>
2017-05-15 22:43:10 -0700

    add weathercard and obj

---
+++ WeatherSimulator/index.vr.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+import { View, Text, Pano, AppRegistry, asset } from 'react-vr';
+
+import WeatherCard from './vr/components/WeatherCard';
+
+const api_key = 'd949c020879a0b22eb58441bee9551fd';
+
+class WeatherSimulator extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      weatherObject: {
+        name: '',
+        main: {
+          temp: 0
+        },
+        weather: [
+          {description: ''}
+        ],
+        wind: {
+          deg: 1,
+          speed: 1
+        }
+      }
+    }
+  }
+
+
+  componentDidMount() {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=SanFrancisco&appid=${api_key}`, {
+      method: 'GET'
+    })
+      .then(response => response.json())
+      .then(json => this.setState({weatherObject: json}));
+  }
+
+  render() {
+    return (
+      <View style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'}
+      }>
+        <Pano source={asset('lombard-vr.jpg')}></Pano>
+        <WeatherCard weatherObject={this.state.weatherObject}/>
+      </View>
+    )
+  }
+};
+
+AppRegistry.registerComponent('WeatherSimulator', () => WeatherSimulator);
---
+++ WeatherSimulator/vr/components/WeatherCard.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+
+import { View, Text, Image, StyleSheet } from 'react-vr';
+
+export default class WeatherCard extends Component {
+  render() {
+    return (
+      <View style={styles.weatherCard}>
+        <Text style={styles.weatherText}>{this.props.weatherObject.name}</Text>
+        <Text style={styles.weatherText}>Current Weather: {this.props.weatherObject.weather[0].description}</Text>
+        <Text style={styles.weatherText}>Temperature: {this.props.weatherObject.main.temp}°</Text>
+        <Text style={styles.weatherText}>Wind Speed: {this.props.weatherObject.wind.speed}mph</Text>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  weatherCard: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+    opacity: 0.8,
+    borderRadius: 0.1,
+    borderColor: '#000',
+    borderWidth: 0.05,
+    padding: 0.5,
+    layoutOrigin: [-0.5, 0],
+    transform: [
+      {translate: [-3, 0, -7]}
+    ]
+  },
+  weatherText: {
+    textAlign: 'center',
+    fontSize: 0.3,
+    color: '#000'
+  }
+})
