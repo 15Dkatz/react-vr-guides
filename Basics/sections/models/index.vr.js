7b25c02ef25293d52d395895010dbb6d60636af1
davidkatz <15Dkatz@shcp.edu>
2017-05-15 20:20:11 -0700

    add models example

--- Basics/sections/models/index.vr.js
+++ Basics/sections/models/index.vr.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 
-import { AppRegistry, asset, Pano, View, Text } from 'react-vr';
+import { AppRegistry, asset, Pano, View, Text, Model } from 'react-vr';
 
 export default class Basics extends Component {
   render() {
     return (
       <View>
         <Pano source={asset('starry-sky.jpg')}></Pano>
+        <Model
+          source={{obj: asset('Tree.obj')}}
+          style={{transform: [{translate: [0, -1, -3]}]}}
+          texture={'http://i.imgur.com/14HMTcQb.jpg'}
+        />
       </View>
     )
   }
