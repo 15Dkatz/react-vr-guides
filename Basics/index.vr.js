42cdf92596138515057aa729f216cc27fb92aadf
davidkatz <15Dkatz@shcp.edu>
2017-05-15 20:25:58 -0700

    add moon

--- Basics/index.vr.js
+++ Basics/index.vr.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 
-import { AppRegistry, asset, Pano, View, Text } from 'react-vr';
+import { AppRegistry, asset, Pano, View, Text, Animated } from 'react-vr';
 
 export default class Basics extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      zValue: new Animated.Value(-2)
+    }
+  }
+
   render() {
     return (
       <View>
